470b1e298f7dbdd1084f7d6283328fa3d3746987
TTeven <16635670@students.lincoln.ac.uk>
2017-12-07 00:23:20 +0000

    i actually started the website

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$(function(){
+$(".aboutus").css("display", "block");
+$(".aboutus").fadeOut(0);
+var currentpage = 1;
+console.log("Ready");
+$("#button1").click(function(){
+                    
+    if (currentpage === 1)
+    {
+        //:thinking emoji:
+    }
+    if (currentpage === 2)
+    {
+        $(".aboutus").fadeOut("slow");
+    }
+    if (currentpage === 3)
+    {
+        
+    }
+    if (currentpage === 4)
+    {
+        
+    }
+    $("#button1").css("content", "url('/assets/button_highlight.png')");
+    $(".homepage").fadeIn("slow");
+    //console.log($(".aboutus").css("display"));
+    
+    
+    currentpage = 1;
+});
+$("#button2").click(function(){
+    if (currentpage === 1)
+    {
+        $(".homepage").fadeOut("slow");
+    }
+    if (currentpage === 2)
+    {
+        //:thinking emoji:
+    }
+    if (currentpage === 3)
+    {
+        
+    }
+    if (currentpage === 4)
+    {
+        
+    }
+    $("#button2").css("content", "url('/assets/button_highlight.png')");
+    $(".aboutus").fadeIn("slow");
+    //console.log($(".aboutus").css("display"));
+    
+    
+    currentpage = 2;
+});
+    
+});
+
+ 
