e1a579ecef7c2ee6e28ffa412aa201022be1a5dc
TTeven <16635670@students.lincoln.ac.uk>
2017-12-15 00:17:06 +0000

    l e m o n s

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(function(){
 $(".aboutus").css("display", "block");
 $(".aboutus").fadeOut(0);
-$("#bottext1").fadeTo(100, 0.00);
-$("#bottext2").fadeTo(100, 0.00);
-$("#bottext3").fadeTo(100, 0.00);
-$("#bottext4").fadeTo(100, 0.00);
-$("#bottext5").fadeTo(100, 0.00);
+$(".themes").css("display", "inline");
+$(".themes").fadeOut(0);
+$("#bottext1").fadeTo(100, 0.10);
+$("#bottext2").fadeTo(100, 0.10);
+$("#bottext3").fadeTo(100, 0.10);
+$("#bottext4").fadeTo(100, 0.10);
+$("#bottext5").fadeTo(100, 0.10);
 var currentpage = 1;
 console.log("Ready");
 //Yes i understand this code is extremely inefficent - but it is the only way i have found to work so far
@@ -13,49 +15,47 @@ $("#button1").mouseenter(function(){
    $("#bottext1").fadeTo(100, 1); 
 });
 $("#button1").mouseleave(function(){
-   $("#bottext1").fadeTo(100, 0.00); 
+   $("#bottext1").fadeTo(100, 0.10); 
 });
 $("#button2").mouseenter(function(){
    $("#bottext2").fadeTo(100, 1); 
 });
 $("#button2").mouseleave(function(){
-   $("#bottext2").fadeTo(100, 0.00); 
+   $("#bottext2").fadeTo(100, 0.10); 
 });
 $("#button3").mouseenter(function(){
    $("#bottext3").fadeTo(100, 1); 
 });
 $("#button3").mouseleave(function(){
-   $("#bottext3").fadeTo(100, 0.00); 
+   $("#bottext3").fadeTo(100, 0.10); 
 });
 $("#button4").mouseenter(function(){
    $("#bottext4").fadeTo(100, 1); 
 });
 $("#button4").mouseleave(function(){
-   $("#bottext4").fadeTo(100, 0.00); 
+   $("#bottext4").fadeTo(100, 0.10); 
 });
 $("#button5").mouseenter(function(){
    $("#bottext5").fadeTo(100, 1); 
 });
 $("#button5").mouseleave(function(){
-   $("#bottext5").fadeTo(100, 0.00); 
+   $("#bottext5").fadeTo(100, 0.10); 
 });
 $("#button1").click(function(){
-                    
-    if (currentpage === 1)
-    {
-        //:thinking emoji:
-    }
-    if (currentpage === 2)
-    {
-        $(".aboutus").fadeOut("slow");
-    }
-    if (currentpage === 3)
-    {
-        
-    }
-    if (currentpage === 4)
-    {
-        
+    switch(currentpage){
+        case 1:
+            break;
+        case 2:
+            $(".aboutus").fadeOut("slow");
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            $(".themes").fadeOut("slow");
+            break;
+        case 5:
+
     }
     $(".homepage").fadeIn("slow");
     //console.log($(".aboutus").css("display"));
@@ -64,21 +64,21 @@ $("#button1").click(function(){
     currentpage = 1;
 });
 $("#button2").click(function(){
-    if (currentpage === 1)
-    {
-        $(".homepage").fadeOut("slow");
-    }
-    if (currentpage === 2)
-    {
-        //:thinking emoji:
-    }
-    if (currentpage === 3)
-    {
-        
-    }
-    if (currentpage === 4)
-    {
-        
+    switch(currentpage){
+        case 1:
+            $(".homepage").fadeOut("slow");
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            $(".themes").fadeOut("slow");
+            break;
+        case 5:
+
     }
     $(".aboutus").fadeIn("slow");
     //console.log($(".aboutus").css("display"));
@@ -86,7 +86,33 @@ $("#button2").click(function(){
     
     currentpage = 2;
 });
+$("#button4").click(function(){
+    switch(currentpage){
+        case 1:
+            $(".homepage").fadeOut("slow");
+            break;
+        case 2:
+            $(".aboutus").fadeOut("slow");
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            break;
+        case 5:
+
+    }
+    $(".themes").fadeIn("slow");
+    //console.log($(".aboutus").css("display"));
     
+    
+    currentpage = 4;
+});
+    
+$("#themeselect2").click(function(){
+   $("body").css("background", 'url("../assets/wlgyl.png") no-repeat center center fixed');
+   $(".blur").css("background", 'url("../assets/wlgyl.png") no-repeat center center fixed');
+});
 });
 
  
